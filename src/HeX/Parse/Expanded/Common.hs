77aa794ffdad9dc1a89073c1e5d5f55f38be0263
Elliot Marsden <elliot.marsden@gmail.com>
2018-09-01 12:06:47 +0200

    Move parsing primitives back to Common module
    By generalising them to work on all streams of primitive tokens, rather than
    just ExpandedStream.

--- src/HeX/Parse/Expanded/Common.hs
+++ src/HeX/Parse/Expanded/Common.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module HeX.Parse.Expanded.Common where
 
-import qualified Data.Char as C
+import Data.Char (chr, ord, toLower, toUpper)
 import Data.Functor (($>))
 import Data.Maybe (isJust)
 import qualified Text.Megaparsec as P
@@ -11,7 +12,63 @@ import qualified HeX.Lex as Lex
 
 import HeX.Parse.Helpers
 import HeX.Parse.Lexed
+import HeX.Parse.Resolved (PrimitiveToken)
 import qualified HeX.Parse.Resolved as R
 
-import HeX.Parse.Expanded.Stream
+isTokenForFont :: PrimitiveToken -> Bool
+isTokenForFont (R.TokenForFont _) = True
+isTokenForFont _ = False
 
+-- <space token> = character token of category [space], or a control sequence
+-- or active character \let equal to such.
+isSpace :: PrimitiveToken -> Bool
+isSpace = isCategory Lex.Space
+
+isActiveCharacter :: PrimitiveToken -> Bool
+isActiveCharacter = isCategory Lex.Active
+
+isNonActiveCharacter :: PrimitiveToken -> Bool
+isNonActiveCharacter = not . isActiveCharacter
+
+isEquals :: PrimitiveToken -> Bool
+isEquals (R.CharCat Lex.CharCat {cat = Lex.Other, char = 61}) = True
+isEquals _ = False
+
+isCategory :: Lex.LexCatCode -> PrimitiveToken -> Bool
+isCategory c (R.CharCat Lex.CharCat {cat = c'}) = c == c'
+isCategory _ _ = False
+
+isLetter :: PrimitiveToken -> Bool
+isLetter = isCategory Lex.Letter
+
+isOther :: PrimitiveToken -> Bool
+isOther = isCategory Lex.Other
+
+isLetterOrOther :: PrimitiveToken -> Bool
+isLetterOrOther x = isLetter x || isOther x
+
+isExplicitLeftBrace :: PrimitiveToken -> Bool
+isExplicitLeftBrace = isCategory Lex.BeginGroup
+
+matchNonActiveCharacterUncased :: Char -> PrimitiveToken -> Bool
+matchNonActiveCharacterUncased a t@(R.CharCat Lex.CharCat {char = c}) =
+  isNonActiveCharacter t && (chr c `elem` [toUpper a, toLower a])
+matchNonActiveCharacterUncased _ _ = False
+
+skipOneOptionalSpace :: (P.Stream s, P.Token s ~ PrimitiveToken) => NullSimpParser s
+skipOneOptionalSpace = skipOneOptionalSatisfied isSpace
+
+-- <optional spaces> = <zero or more spaces>.
+skipOptionalSpaces :: (P.Stream s, P.Token s ~ PrimitiveToken) => NullSimpParser s
+skipOptionalSpaces = skipManySatisfied isSpace
+
+skipKeyword :: (P.Stream s, P.Token s ~ PrimitiveToken) => String -> NullSimpParser s
+skipKeyword s =
+  skipOptionalSpaces *>
+  mapM_ (skipSatisfied . matchNonActiveCharacterUncased) s
+
+parseOptionalKeyword :: (P.Stream s, P.Token s ~ PrimitiveToken) => String -> SimpParser s Bool
+parseOptionalKeyword s = isJust <$> P.optional (P.try $ skipKeyword s)
+
+parseKeywordToValue :: (P.Stream s, P.Token s ~ PrimitiveToken) => String -> b -> SimpParser s b
+parseKeywordToValue s = (skipKeyword s $>)
--- src/HeX/Parse/Expanded/Stream.hs
+++ src/HeX/Parse/Expanded/Stream.hs
@@ -5,8 +5,6 @@ module HeX.Parse.Expanded.Stream where
 
 import Data.Char (chr, ord, toLower, toUpper)
 import Data.Proxy
-import Data.Maybe (isJust)
-import Data.Functor (($>))
 import qualified Text.Megaparsec as P
 
 import HeX.Categorise (CharCode)
@@ -19,6 +17,8 @@ import HeX.Parse.Lexed (BalancedText(..))
 import HeX.Parse.Resolved (PrimitiveToken)
 import HeX.Parse.Resolved as R
 
+import HeX.Parse.Expanded.Common
+
 newtype ExpandedStream =
   ExpandedStream R.ResolvedStream
 
@@ -30,6 +30,7 @@ type PrimitiveTokens = [PrimitiveToken]
 -- Set the character code of each character token to its
 -- \uccode or \lccode value, if that value is non-zero.
 -- Don't change the category code.
+changeCase :: VDirection -> Lex.Token -> Lex.Token
 changeCase dir (Lex.CharCatToken (Lex.CharCat char cat)) =
   Lex.CharCatToken $ Lex.CharCat (modChar char) cat
   where
@@ -39,17 +40,20 @@ changeCase dir (Lex.CharCatToken (Lex.CharCat char cat)) =
 changeCase _ t = t
 
 
-
+-- Things I can't easily parse outside this module, because of the recursive
+-- parsing in ExpandedStream.
+-- Constraining only the stream token type won't work easily either, because
+-- the functions currently depend on the ExpandedStream type per se.
 parseInhibited :: SimpLexParser a -> SimpExpandParser a
 parseInhibited p = do
   P.State {stateInput = ExpandedStream (R.ResolvedStream lStream csMap)} <- P.getParserState
   case easyRunParser p lStream of
     (_, Left _) -> error "ohnoes"
-    (P.State lStr pos prc w, Right v) -> do
-      P.setParserState (P.State (ExpandedStream $ R.ResolvedStream lStr csMap) pos prc w)
+    (P.State lStream' pos prc w, Right v) -> do
+      P.setParserState (P.State (ExpandedStream $ R.ResolvedStream lStream' csMap) pos prc w)
       return v
 
-parseGeneralText :: SimpExpandParser BalancedText
+parseGeneralText :: SimpParser ExpandedStream BalancedText
 parseGeneralText = do
   skipManySatisfied isFillerItem
   -- TODO: Maybe other things can act as left braces.
@@ -59,66 +63,7 @@ parseGeneralText = do
     isFillerItem R.Relax = True
     isFillerItem t = isSpace t
 
-skipOneOptionalSpace :: NullSimpParser ExpandedStream
-skipOneOptionalSpace = skipOneOptionalSatisfied isSpace
-
--- <optional spaces> = <zero or more spaces>.
-skipOptionalSpaces :: NullSimpParser ExpandedStream
-skipOptionalSpaces = skipManySatisfied isSpace
-
-isTokenForFont :: MatchToken ExpandedStream
-isTokenForFont (R.TokenForFont _) = True
-isTokenForFont _ = False
-
--- <space token> = character token of category [space], or a control sequence
--- or active character \let equal to such.
-isSpace :: MatchToken ExpandedStream
-isSpace = isCategory Lex.Space
-
-isActiveCharacter :: MatchToken ExpandedStream
-isActiveCharacter = isCategory Lex.Active
-
-isNonActiveCharacter :: MatchToken ExpandedStream
-isNonActiveCharacter = not . isActiveCharacter
-
-isEquals :: MatchToken ExpandedStream
-isEquals (R.CharCat Lex.CharCat {cat = Lex.Other, char = 61}) = True
-isEquals _ = False
-
-isCategory :: Lex.LexCatCode -> MatchToken ExpandedStream
-isCategory c (R.CharCat Lex.CharCat {cat = c'}) = c == c'
-isCategory _ _ = False
-
-isLetter :: MatchToken ExpandedStream
-isLetter = isCategory Lex.Letter
-
-isOther :: MatchToken ExpandedStream
-isOther = isCategory Lex.Other
-
-isLetterOrOther :: MatchToken ExpandedStream
-isLetterOrOther x = isLetter x || isOther x
-
-isExplicitLeftBrace :: MatchToken ExpandedStream
-isExplicitLeftBrace = isCategory Lex.BeginGroup
-
-matchNonActiveCharacterUncased :: Char -> MatchToken ExpandedStream
-matchNonActiveCharacterUncased a t@(R.CharCat Lex.CharCat {char = c}) =
-  isNonActiveCharacter t && (chr c `elem` [toUpper a, toLower a])
-matchNonActiveCharacterUncased _ _ = False
-
-skipKeyword :: String -> NullSimpParser ExpandedStream
-skipKeyword s =
-  skipOptionalSpaces *>
-  mapM_ (skipSatisfied . matchNonActiveCharacterUncased) s
-
-parseOptionalKeyword :: String -> SimpExpandParser Bool
-parseOptionalKeyword s = isJust <$> P.optional (P.try $ skipKeyword s)
-
-parseKeywordToValue :: String -> b -> SimpExpandParser b
-parseKeywordToValue s = (skipKeyword s $>)
-
-
-parseChangeCase :: SimpExpandParser BalancedText
+parseChangeCase :: SimpParser ExpandedStream BalancedText
 parseChangeCase = parseGeneralText
 
 
@@ -173,6 +118,9 @@ instance P.Stream ExpandedStream where
       SyntaxCommandHead (MacroToken (Macro [] (BalancedText mToks))) ->
             P.take1_ $
             insertLexTokensE (ExpandedStream rs') mToks
+      -- SyntaxCommandHead CSName ->
+      --   case easyRunParser parseCSName (ExpandedStream rs') of
+
 
 type SimpExpandParser = P.Parsec () ExpandedStream
 
