e9bc5e88241266269f45ddad2e0feb4cf4b89608
Elliot Marsden <elliot.marsden@gmail.com>
2018-11-17 21:44:10 +0100

    Clean up seemingly final 'What' breaking algorithm

--- app/What.hs
+++ app/What.hs
@@ -1,25 +1,16 @@
 {-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
-import GHC.Generics (Generic)
-import Data.Hashable
 import Data.List.Extra
-import           Data.Vector ((!))
-import qualified Data.Vector as V
 
-import Debug.Trace
+data Entry = Char String | Break
+    deriving Show
 
-data Entry = Char Char | Break
-    deriving (Show, Generic)
+type EdgeDistance = Int
 
-instance Hashable Entry
-
-type FiniteEdgeDistance = Int
-
-data EdgeDistance
-    = Acceptable FiniteEdgeDistance
+data EdgeStatus
+    = Acceptable EdgeDistance
     | TooFull
     | TooBare
     deriving Show
@@ -29,13 +20,13 @@ type EdgeValue = [Entry]
 data Node = Root | Branch Int
     deriving Show
 
-type InEdge = (EdgeValue, Node)
-type DInEdge = (InEdge, Int)
-type DInEdgeList = [DInEdge]
-type Graph = [DInEdgeList]
-type VGraph = V.Vector DInEdgeList
+data InEdge = InEdge EdgeValue Node
 
-data Route = Route [EdgeValue] FiniteEdgeDistance
+data WithSummary a b = WithSummary {value :: a, summary :: b}
+type WithDistance a = WithSummary a EdgeDistance
+type WithStatus a = WithSummary a EdgeStatus
+
+data Route = Route [EdgeValue] EdgeDistance
     deriving Show
 
 instance Eq Route where
@@ -44,113 +35,111 @@ instance Eq Route where
 instance Ord Route where
     compare (Route _ a) (Route _ b) = compare a b
 
-edgeDistance :: EdgeValue -> EdgeDistance
-edgeDistance (length -> x)
+judgeEdge :: EdgeValue -> EdgeStatus
+judgeEdge (length -> x)
     | x < 90 = TooBare
     | x > 110 = TooFull
     | otherwise = Acceptable $ 2000 - x ^ (2 :: Int)
 
--- edgeDistance :: EdgeValue -> EdgeDistance
--- edgeDistance (length -> x)
---     | x == 1 = TooBare
---     | x == 2 = Acceptable 1
---     | x == 3 = Acceptable 2
---     | otherwise = TooFull
-
-withDistance :: InEdge -> (InEdge, EdgeDistance)
-withDistance e@(v, _) = (e,  edgeDistance v)
-
 inputRaw :: String
-inputRaw = "C Biodiesel blog migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog  migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog  migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse."
+inputRaw = "C Biodiesel blog migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog  migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog  migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog  migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog  migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog  migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog  migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog  migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse. C Biodiesel blog  migas subway tile wayfarers vinyl before they sold out listicle mlkshk kitsch. Food truck vegan synth street art, tofu organic marfa subway tile drinking vinegar. Put a bird on it skateboard offal four loko lo-fi lumbersexual. Actually pabst selfies austin single-origin coffee, mlkshk pork belly whatever lo-fi YOLO skateboard fanny pack thundercats truffaut hammock. Williamsburg next level banh mi blog 90's. Austin next level celiac pok pok, keffiyeh williamsburg health goth brooklyn kinfolk. Coloring book palo santo everyday carry YOLO bicycle rights chartreuse."
 
 input :: [Entry]
 input = convert <$> inputRaw
     where
         convert ' ' = Break
-        convert c = Char c
+        convert c = Char [c]
 
--- input = [Char 'a', Char 'a', Break, Char 'b', Char 'b', Break, Char 'c', Char 'c']
+-- judgeEdge :: EdgeValue -> EdgeStatus
+-- judgeEdge (length -> x)
+--     | x == 1 = Acceptable 0
+--     | x == 2 = Acceptable 1
+--     | x == 3 = Acceptable 2
+--     | x == 4 = Acceptable 0
+--     | otherwise = TooFull
 
-toOnlyAcceptables :: [(a, EdgeDistance)] -> [(a, FiniteEdgeDistance)]
-toOnlyAcceptables ds = [(y, x) | (y, Acceptable x) <- ds]
+-- input = [Char "a1", Char "a2", Break, Char "b1", Char "b2", Break, Char "c1", Char "c2"]
 
-toOnlyPromisings :: [(a, EdgeDistance)] -> [(a, EdgeDistance)]
-toOnlyPromisings ds = [(y, d) | (y, d) <- ds, isPromising d]
+
+
+withStatus :: InEdge -> WithStatus InEdge
+withStatus e@(InEdge v _) = WithSummary e $ judgeEdge v
+
+toOnlyAcceptables :: [WithStatus InEdge] -> [WithDistance InEdge]
+toOnlyAcceptables ds = [WithSummary y x | (WithSummary y (Acceptable x)) <- ds]
+
+toOnlyPromisings :: [WithStatus a] -> [WithStatus a]
+toOnlyPromisings ds = [WithSummary y d | (WithSummary y d) <- ds, isPromising d]
     where
         isPromising TooFull = False
         isPromising _ = True
 
-growInEdge :: a -> ([a], b) -> ([a], b)
-growInEdge x (cs, n) = (x:cs, n)
+growInEdge :: Entry -> InEdge -> InEdge
+growInEdge c (InEdge cs n) = InEdge (c : cs) n
 
-appendEntry :: ([InEdge], [Route]) -> Entry -> ([InEdge], [Route])
-appendEntry (inEdges, rs) x@Break =
+stretchInEdge :: [Entry] -> InEdge -> InEdge
+stretchInEdge s (InEdge cs n) = InEdge (s ++ cs) n
+
+appendEntry :: ([InEdge], [Route], [Entry]) -> Entry -> ([InEdge], [Route], [Entry])
+appendEntry (prevInEdges, rs, chunk) x@Break =
     -- Reached a break point.
     -- Plan:
     -- - Remove too long accumulated lines.
     -- - Insert acceptable lines, with the associated predecessor breaks, as the result for this break.
     -- - Add another line for the next break, to represent the possibility of this break being the predecessor.
     let
+        -- Get the candidates for this break, by appending the chunk we just grabbed to our previous candidate edges.
+        inEdges = stretchInEdge chunk <$> prevInEdges
         -- Compute edge distances, and throw out too long ones.
-        promisingDInEdges = toOnlyPromisings $ withDistance <$> inEdges
-        -- Of these promising edges, append the current character, to form the
-        -- candidates for the next break.
-        grownPromisingInEdges = growInEdge x . fst <$> promisingDInEdges
+        promisingDInEdges = toOnlyPromisings $ withStatus <$> inEdges
+        promisingInEdges = value <$> promisingDInEdges
     in
         -- Find the subset of promising edges that are acceptable right now.
         case toOnlyAcceptables promisingDInEdges of
             -- If we can't break at this point acceptably somehow, just treat
             -- like a non-break character.
-            [] -> (grownPromisingInEdges, rs)
+            [] -> (promisingInEdges, rs, [x])
             acceptableDInEdges ->
                 let
+                    -- Of the promising edges, append the current character, to form the
+                    -- candidates for the next break.
+                    grownPromisingInEdges = growInEdge x <$> promisingInEdges
                     -- Candidate edges for the next break are:
                     -- - Already promising edges, with this break-point item appended
                     -- - A new edge to hold the line between this break and the next.
-                    newInEdges = ([], Branch $ length rs) : grownPromisingInEdges
+                    newInEdges = InEdge [] (Branch $ length rs) : grownPromisingInEdges
                     -- Find the best route to this node, and add it to our node
                     -- list.
-                    r' = shortestRoute' acceptableDInEdges rs
+                    r' = shortestRoute acceptableDInEdges rs
                 in
-                    (newInEdges, r':rs)
-appendEntry (inEdges, rs) x =
-    (growInEdge x <$> inEdges, rs)
+                    (newInEdges, r':rs, [])
+appendEntry (prevInEdges, rs, chunk) x =
+    (prevInEdges, rs, x:chunk)
 
 bestRoute :: [Entry] -> Route
 bestRoute xs =
     let
-        (inEdges, rs) = foldl' appendEntry ([([], Root)], []) xs
+        (prevInEdges, rs, chunk) = foldl' appendEntry ([InEdge [] Root], [], []) xs
         -- Force a final 'break' representing the end of the paragraph.
-        acceptableDInEdges = toOnlyAcceptables $ withDistance <$> inEdges
-    in shortestRoute' acceptableDInEdges rs
+        inEdges = stretchInEdge chunk <$> prevInEdges
+        acceptableDInEdges = toOnlyAcceptables $ withStatus <$> inEdges
+    in shortestRoute acceptableDInEdges rs
 
-shortestRoute' :: [DInEdge] -> [Route] -> Route
-shortestRoute' ies rs =
-    case getBSR <$> ies of
-        [] -> Route [] 0
-        sths -> minimum sths
+shortestRoute :: [WithDistance InEdge] -> [Route] -> Route
+shortestRoute [] _ = Route [] 0
+shortestRoute ies rs = minimum (getBSR <$> ies)
     where
-        getBSR ((ev, Root), ed)
+        getBSR (WithSummary (InEdge ev Root) ed)
             = Route [ev] ed
-        getBSR ((ev, Branch sn), ed)
+        getBSR (WithSummary (InEdge ev (Branch sn)) ed)
             = let
                 sourceNodeIdx = length rs - sn - 1
                 Route sevs sd = rs !! sourceNodeIdx
             in
                 Route (ev:sevs) (ed + sd)
 
-showOptions :: (Int, [(InEdge, Int)]) -> String
-showOptions (n, xs) = show n ++ "\n" ++ intercalate "\n" line
-    where
-        line = ("\t" ++) . showEdge <$> xs
-        showEdge ((_, sn), ed) = show (sn, ed)
-
 main :: IO ()
 main = do
     let r = bestRoute input
-    -- putStrLn $ intercalate "\n\n" $ showOptions <$> zip (reverse [0 .. length g - 1]) g
-
-    -- putStrLn "\n"
-
     r `seq` return ()
     -- print r
