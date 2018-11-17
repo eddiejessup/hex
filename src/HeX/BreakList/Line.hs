bd0c5be6cf48d3236eeb11d8be8b74f7c0882a72
Elliot Marsden <elliot.marsden@gmail.com>
2018-11-17 22:52:28 +0100

    Record in-construction but working line break rewrite

--- src/HeX/BreakList/Line.hs
+++ src/HeX/BreakList/Line.hs
@@ -6,6 +6,7 @@ module HeX.BreakList.Line where
 
 import           Data.Maybe
 import           Prelude                 hiding ( lines )
+import Data.List.Extra hiding (lines)
 
 import           Control.Applicative            ( liftA2 )
 import           Data.List                      ( intercalate )
@@ -122,13 +123,107 @@ bestRouteInner desiredWidth tolerance linePenalty cs
         listDemerit  = (linePenalty + bad) ^ (2 :: Int)
     in  breakDemerit + listDemerit
 
+
+-- *** Under construction ***
+
+judgeEdge :: EdgeValue -> EdgeStatus
+judgeEdge (length -> x)
+    | x < 90 = TooBare
+    | x > 110 = TooFull
+    | otherwise = Acceptable 100
+
+type Entry = A.Adjacency BreakableHListElem
+type EdgeDistance = Int
+data EdgeStatus = TooBare | TooFull | Acceptable EdgeDistance
+type EdgeValue = [Entry]
+
+data Node = Root | Branch Int
+    deriving Show
+
+data InEdge = InEdge EdgeValue Node
+
+data WithSummary a b = WithSummary {value :: a, summary :: b}
+type WithDistance a = WithSummary a EdgeDistance
+type WithStatus a = WithSummary a EdgeStatus
+
+data Route' = Route' [EdgeValue] EdgeDistance
+    deriving Show
+
+instance Eq Route' where
+    (Route' _ a) == (Route' _ b) = a == b
+
+instance Ord Route' where
+    compare (Route' _ a) (Route' _ b) = compare a b
+
+withStatus :: InEdge -> WithStatus InEdge
+withStatus e@(InEdge v _) = WithSummary e $ judgeEdge v
+
+toOnlyAcceptables :: [WithStatus InEdge] -> [WithDistance InEdge]
+toOnlyAcceptables ds = [WithSummary y x | (WithSummary y (Acceptable x)) <- ds]
+
+toOnlyPromisings :: [WithStatus a] -> [WithStatus a]
+toOnlyPromisings ds = [WithSummary y d | (WithSummary y d) <- ds, isPromising d]
+    where
+        isPromising TooFull = False
+        isPromising _ = True
+
+growInEdge :: Entry -> InEdge -> InEdge
+growInEdge c (InEdge cs n) = InEdge (c : cs) n
+
+stretchInEdge :: [Entry] -> InEdge -> InEdge
+stretchInEdge s (InEdge cs n) = InEdge (s ++ cs) n
+
+appendEntry :: ([InEdge], [Route'], [Entry]) -> Entry -> ([InEdge], [Route'], [Entry])
+appendEntry (prevInEdges, rs, chunk) x@(toBreakItem -> Nothing) =
+  (prevInEdges, rs, x:chunk)
+appendEntry (prevInEdges, rs, chunk) x@(toBreakItem -> Just b) =
+  let
+    inEdges = stretchInEdge chunk <$> prevInEdges
+    promisingDInEdges = toOnlyPromisings $ withStatus <$> inEdges
+    promisingInEdges = value <$> promisingDInEdges
+  in
+    case toOnlyAcceptables promisingDInEdges of
+      -- If we can't break at this point acceptably somehow, just treat
+      -- like a non-break character.
+      -- [] -> (promisingInEdges, rs, [x])
+      [] -> (promisingInEdges, rs, [x])
+      acceptableDInEdges ->
+        let
+            grownPromisingInEdges = growInEdge x <$> promisingInEdges
+            newInEdges = InEdge [] (Branch $ length rs) : grownPromisingInEdges
+            r' = shortestRoute acceptableDInEdges rs
+        in
+            (newInEdges, r':rs, [])
+
+shortestRoute :: [WithDistance InEdge] -> [Route'] -> Route'
+shortestRoute [] _ = Route' [] 0
+shortestRoute ies rs = minimum (getBSR <$> ies)
+    where
+        getBSR (WithSummary (InEdge ev Root) ed)
+            = Route' [ev] ed
+        getBSR (WithSummary (InEdge ev (Branch sn)) ed)
+            = let
+                sourceNodeIdx = length rs - sn - 1
+                Route' sevs sd = rs !! sourceNodeIdx
+            in
+                Route' (ev:sevs) (ed + sd)
+
 bestRoute :: Int -> Int -> Int -> [BreakableHListElem] -> [Line]
-bestRoute x y z cs = lines $ bestRouteInner x y z cs
+bestRoute desiredWidth tolerance linePenalty xs =
+  let
+    (prevInEdges, rs, chunk) = foldl' appendEntry ([InEdge [] Root], [], []) (A.toAdjacents xs)
+    inEdges = stretchInEdge chunk <$> prevInEdges
+    acceptableDInEdges = toOnlyAcceptables $ withStatus <$> inEdges
+    (Route' evs _) = shortestRoute acceptableDInEdges rs
+  in
+    (\ev -> Line (reverse ev) NaturallyGood) . A.fromAdjacents <$> reverse evs
+    -- [Line (take 10 xs) NaturallyGood, Line (take 20 xs) NaturallyGood]
+
+-- *** Under construction ***
 
 -- Expects contents in reverse order.
 -- Returns lines in reading order.
-setParagraph
-  :: ([BreakableHListElem] -> [Line]) -> [BreakableHListElem] -> [[B.HBoxElem]]
+setParagraph :: ([BreakableHListElem] -> [Line]) -> [BreakableHListElem] -> [[B.HBoxElem]]
 setParagraph _        []              = []
 setParagraph getRoute cs@(end : rest) = fmap setLine lns
     -- Remove the final item if it's glue.
