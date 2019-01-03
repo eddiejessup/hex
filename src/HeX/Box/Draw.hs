32793083b672777315d2cfe79f72463559cb3603
Elliot Marsden <elliot.marsden@gmail.com>
2019-01-04 00:55:39 +0100

    Reformat Box.Draw

--- src/HeX/Box/Draw.hs
+++ src/HeX/Box/Draw.hs
@@ -8,34 +8,41 @@ import qualified DVI.Document                  as D
 
 import           HeX.Box
 
-boxContentsToDVI :: BoxContents -> [D.Instruction]
-boxContentsToDVI (HBoxContents cs) = concatMap hBoxElemToDVI cs
-boxContentsToDVI (VBoxContents cs) = concatMap vBoxElemToDVI cs
-
 boxToDVI :: Box -> [D.Instruction]
-boxToDVI Box {contents = cs} = boxContentsToDVI cs
+boxToDVI Box { contents = cs } = case cs of
+    HBoxContents elems -> concatMap hBoxElemToDVI elems
+    VBoxContents elems -> concatMap vBoxElemToDVI elems
 
 hBoxElemToDVI :: HBoxElem -> [D.Instruction]
-hBoxElemToDVI (HVBoxElem (BoxChild b)) =
-  [D.PushStack] ++ boxToDVI b ++ [D.PopStack, D.Move Horizontal $ naturalWidth b]
-hBoxElemToDVI (HVBoxElem (BoxGlue g)) = [D.Move Horizontal $ glueDimen g]
-hBoxElemToDVI (HVBoxElem (BoxKern k)) = [D.Move Horizontal $ kernDimen k]
-hBoxElemToDVI (HVBoxElem (BoxRule r)) =
-  [D.PushStack] ++ [D.AddRule r] ++ [D.PopStack, D.Move Horizontal $ naturalWidth r]
-hBoxElemToDVI (HVBoxElem e@(BoxFontDefinition _)) = vBoxElemToDVI e
-hBoxElemToDVI (HVBoxElem e@(BoxFontSelection _)) = vBoxElemToDVI e
-hBoxElemToDVI (BoxCharacter e) = [D.AddCharacter e]
+hBoxElemToDVI el = case el of
+    HVBoxElem (BoxChild b) ->
+        [D.PushStack]
+        ++ boxToDVI b
+        ++ [D.PopStack, D.Move Horizontal $ naturalWidth b]
+    HVBoxElem (BoxRule b) ->
+        [D.PushStack]
+        ++ [D.AddRule b]
+        ++ [D.PopStack, D.Move Horizontal $ naturalWidth b]
+    HVBoxElem (BoxGlue g)             -> [D.Move Horizontal $ glueDimen g]
+    HVBoxElem (BoxKern k)             -> [D.Move Horizontal $ kernDimen k]
+    HVBoxElem e@(BoxFontDefinition _) -> vBoxElemToDVI e
+    HVBoxElem e@(BoxFontSelection _)  -> vBoxElemToDVI e
+    BoxCharacter e                    -> [D.AddCharacter e]
 
 vBoxElemToDVI :: VBoxElem -> [D.Instruction]
-vBoxElemToDVI (BoxChild b) =
-  [D.PushStack] ++ boxToDVI b ++ [D.PopStack, D.Move Vertical $ naturalHeight b + naturalDepth b]
-  -- TODO: Rule.
-vBoxElemToDVI (BoxGlue g) = [D.Move Vertical $ glueDimen g]
-vBoxElemToDVI (BoxKern k) = [D.Move Vertical $ kernDimen k]
-vBoxElemToDVI (BoxRule r) =
-  [D.PushStack] ++ [D.AddRule r] ++ [D.PopStack, D.Move Vertical $ naturalHeight r + naturalDepth r]
-vBoxElemToDVI (BoxFontDefinition e) = [D.DefineFont e]
-vBoxElemToDVI (BoxFontSelection e) = [D.SelectFont e]
+vBoxElemToDVI el = case el of
+    BoxChild b ->
+        [D.PushStack]
+        ++ boxToDVI b
+        ++ [D.PopStack, D.Move Vertical $ naturalHeight b + naturalDepth b]
+    BoxRule b ->
+        [D.PushStack]
+        ++ [D.AddRule b]
+        ++ [D.PopStack, D.Move Vertical $ naturalHeight b + naturalDepth b]
+    BoxGlue g           -> [D.Move Vertical $ glueDimen g]
+    BoxKern k           -> [D.Move Vertical $ kernDimen k]
+    BoxFontDefinition e -> [D.DefineFont e]
+    BoxFontSelection e  -> [D.SelectFont e]
 
 pageToDVI :: Page -> [D.Instruction]
 pageToDVI (Page vs) = D.BeginNewPage : concatMap vBoxElemToDVI vs
