6c77f095d040a25832c3c6f00a4845bd02fda7ad
Elliot Marsden <elliot.marsden@gmail.com>
2019-01-10 10:16:08 +0100

    Reformat assignment module, and switch bool for Global token

--- src/HeX/Parse/Assignment.hs
+++ src/HeX/Parse/Assignment.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE TypeFamilies #-}
 
 module HeX.Parse.Assignment where
 
@@ -10,7 +9,7 @@ import           Path                           ( File
                                                 , parseRelFile
                                                 )
 import qualified Text.Megaparsec               as P
-import           Text.Megaparsec               ((<|>))
+import           Text.Megaparsec                ( (<|>) )
 
 import qualified HeX.Lex                       as Lex
 import           HeX.Parse.Helpers
@@ -21,39 +20,39 @@ import           HeX.Parse.VarAssignment
 
 -- AST.
 
-data MacroAssignment
-  = MacroAssignment { name :: Lex.ControlSequenceLike
-                    , contents :: T.MacroContents
-                    , long, outer :: Bool }
-  deriving (Show)
+data MacroAssignment = MacroAssignment
+    { name        :: Lex.ControlSequenceLike
+    , contents    :: T.MacroContents
+    , long, outer :: Bool
+    } deriving (Show)
 
 data AssignmentBody
-  = DefineMacro MacroAssignment
-  -- \| ShortDefine {quantity :: QuantityType, name :: ControlSequenceLike, value :: Int}
-  | SetVariable VariableAssignment
-  -- \| ModifyVariable VariableModificatxion
-  -- \| AssignCode { codeType :: CodeType, codeIndex, value :: Int }
-  -- \| Let { future :: Bool, name :: ControlSequenceLike, target :: Token}
-  -- \| FutureLet { name :: ControlSequenceLike, token1, token2 :: Token}
-  | SelectFont Int
-  -- \| SetFamilyMember {member :: FamilyMember, font :: Font}
-  -- \| SetParShape
-  -- \| Read
-  -- \| DefineBox
-  -- TEMP: Dummy label constructor until properly implemented.
-  | DefineFont Lex.ControlSequenceLike
-               (Path Rel File)
-  -- -- Global assignments.
-  -- \| SetFontAttribute
-  -- \| SetHyphenation
-  -- \| SetBoxSize
-  -- \| SetInteractionMode
-  -- \| SetSpecialVariable
-  deriving (Show)
+    = DefineMacro MacroAssignment
+    -- \| ShortDefine {quantity :: QuantityType, name :: ControlSequenceLike, value :: Int}
+    | SetVariable VariableAssignment
+    -- \| ModifyVariable VariableModificatxion
+    -- \| AssignCode { codeType :: CodeType, codeIndex, value :: Int }
+    -- \| Let { future :: Bool, name :: ControlSequenceLike, target :: Token}
+    -- \| FutureLet { name :: ControlSequenceLike, token1, token2 :: Token}
+    | SelectFont Int
+    -- \| SetFamilyMember {member :: FamilyMember, font :: Font}
+    -- \| SetParShape
+    -- \| Read
+    -- \| DefineBox
+    -- TEMP: Dummy label constructor until properly implemented.
+    | DefineFont Lex.ControlSequenceLike
+                 (Path Rel File)
+    -- -- Global assignments.
+    -- \| SetFontAttribute
+    -- \| SetHyphenation
+    -- \| SetBoxSize
+    -- \| SetInteractionMode
+    -- \| SetSpecialVariable
+    deriving (Show)
 
 data Assignment = Assignment
-  { body :: AssignmentBody
-  , global :: Bool
+  { body   :: AssignmentBody
+  , global :: T.GlobalFlag
   } deriving (Show)
 
 type AssignmentParser = SimpExpandParser Assignment
@@ -62,92 +61,100 @@ parseAssignment :: AssignmentParser
 parseAssignment = parseDefineMacro <|> parseNonMacroAssignment
 
 parseNonMacroAssignment :: AssignmentParser
-parseNonMacroAssignment = do
-  global <- parseGlobal
-  _body <- parseNonMacroAssignmentBody
-  pure $ Assignment _body global
+parseNonMacroAssignment =
+    do
+    _global <- parseGlobal
+    _body <- parseNonMacroAssignmentBody
+    pure $ Assignment _body _global
   where
     -- TODO: Parse globals.
-    parseGlobal = pure True
-    parseNonMacroAssignmentBody
-      = P.choice [ SetVariable <$> parseVariableAssignment
+    parseGlobal = pure T.Global
+    parseNonMacroAssignmentBody =
+        P.choice [ SetVariable <$> parseVariableAssignment
                  , parseTokenForFont
-                 , parseMacroToFont ]
+                 , parseMacroToFont
+                 ]
 
 -- Parse Macro.
 
 parseDefineMacro :: AssignmentParser
-parseDefineMacro = do
-  -- Macro prefixes.
-  prefixes <- P.many $ satisfyThen tokToPrefix
-  -- \def-like thing.
-  (defGlobal, defExpand) <- satisfyThen tokToDef
-  -- Macro's name.
-  cs <- parseCSName
-  -- Parameter text.
-  (preParamToks, paramDelims) <- parseParamText
-  -- TODO: Support expanded-def.
-  when defExpand $ error "expanded-def not implemented"
-  -- Replacement text.
-  replaceToks <- parseMacroText
-  pure $
-    Assignment
-    { body = DefineMacro $ MacroAssignment
-        { name = cs
-        , contents = T.MacroContents preParamToks paramDelims replaceToks
-        , long = T.LongTok `elem` prefixes
-        , outer = T.OuterTok `elem` prefixes
+parseDefineMacro =
+    do
+    -- Macro prefixes.
+    prefixes <- P.many $ satisfyThen tokToPrefix
+    -- \def-like thing.
+    (defGlobalType, defExpandType) <- satisfyThen tokToDef
+    -- Macro's name.
+    cs <- parseCSName
+    -- Parameter text.
+    (preParamToks, paramDelims) <- parseParamText
+    -- TODO: Support expanded-def.
+    when (defExpandType == T.ExpandDef) $ error "expanded-def not implemented"
+    -- Replacement text.
+    replaceToks <- parseMacroText
+    pure Assignment
+        { body = DefineMacro $ MacroAssignment
+            { name = cs
+            , contents = T.MacroContents preParamToks paramDelims replaceToks
+            , long = T.LongTok `elem` prefixes
+            , outer = T.OuterTok `elem` prefixes
+            }
+        , global = case defGlobalType of
+            T.Global                              -> T.Global
+            T.Local | T.GlobalTok `elem` prefixes -> T.Global
+            _                                     -> T.Local
         }
-    , global = defGlobal || T.GlobalTok `elem` prefixes
-    }
   where
     tokToPrefix (T.AssignPrefixTok t) = Just t
     tokToPrefix _ = Nothing
 
-    tokToDef (T.DefineMacroTok global expand) =
-      Just (global == T.Global, expand == T.ExpandDef)
-    tokToDef _ =
-      Nothing
+    tokToDef (T.DefineMacroTok _global expand) = Just (_global, expand)
+    tokToDef _ = Nothing
 
 parseTokenForFont :: SimpExpandParser AssignmentBody
-parseTokenForFont = satisfyThen tokToCom
+parseTokenForFont =
+    satisfyThen tokToCom
   where
     tokToCom (T.TokenForFont n) = Just $ SelectFont n
     tokToCom _ = Nothing
 
 -- \font <control-sequence> <equals> <file-name> <at-clause>
 parseMacroToFont :: SimpExpandParser AssignmentBody
-parseMacroToFont = do
-  skipSatisfiedEquals T.FontTok
-  cs <- parseCSName
-  skipOptionalEquals
-  DefineFont cs <$> parseFileName
-    -- <file name> = <optional spaces> <some explicit letter or digit characters> <space>
+parseMacroToFont =
+    do
+    skipSatisfiedEquals T.FontTok
+    cs <- parseCSName
+    skipOptionalEquals
+    DefineFont cs <$> parseFileName
   where
+    -- <file name> = <optional spaces> <some explicit letter or digit characters> <space>
     parseFileName :: SimpExpandParser (Path Rel File)
-    parseFileName = do
-      skipOptionalSpaces
-      fileName <- P.some $ satisfyThen tokToChar
-      skipSatisfied isSpace
-      case parseRelFile (fileName ++ ".tfm") of
-        Just p -> pure p
-        Nothing -> fail $ "Invalid filename: " ++ fileName ++ ".tfm"
-    tokToChar (T.UnexpandedTok (Lex.CharCatToken (Lex.CharCat c Lex.Letter))) = Just c
+    parseFileName =
+        do
+        skipOptionalSpaces
+        fileName <- P.some $ satisfyThen tokToChar
+        skipSatisfied isSpace
+        case parseRelFile (fileName ++ ".tfm") of
+          Just p -> pure p
+          Nothing -> fail $ "Invalid filename: " ++ fileName ++ ".tfm"
+
+    tokToChar (T.UnexpandedTok (Lex.CharCatToken (Lex.CharCat c Lex.Letter))) =
+        Just c
     -- 'Other' Characters for decimal digits are OK.
     tokToChar (T.UnexpandedTok (Lex.CharCatToken (Lex.CharCat c Lex.Other))) =
-      case c of
-        '0' -> Just c
-        '1' -> Just c
-        '2' -> Just c
-        '3' -> Just c
-        '4' -> Just c
-        '5' -> Just c
-        '6' -> Just c
-        '7' -> Just c
-        '8' -> Just c
-        '9' -> Just c
-      -- Not in the spec, but let's say "/" and "." are OK.
-        '/' -> Just c
-        '.' -> Just c
-        _ -> Nothing
+        case c of
+            '0' -> Just c
+            '1' -> Just c
+            '2' -> Just c
+            '3' -> Just c
+            '4' -> Just c
+            '5' -> Just c
+            '6' -> Just c
+            '7' -> Just c
+            '8' -> Just c
+            '9' -> Just c
+            -- Not in the spec, but let's say "/" and "." are OK.
+            '/' -> Just c
+            '.' -> Just c
+            _ -> Nothing
     tokToChar _ = Nothing
