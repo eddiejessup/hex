97c348300993f38bc0eb41aaba6a762fdb17f3e5
Elliot Marsden <elliot.marsden@gmail.com>
2019-01-11 16:18:01 +0100

    Reformat lex module

--- src/HeX/Lex.hs
+++ src/HeX/Lex.hs
@@ -9,56 +9,55 @@ import           Data.Hashable                  ( Hashable
 import qualified HeX.Categorise                as Cat
 import           HeX.Categorise                 ( CharCode )
 
-newtype ControlSequence =
-  ControlSequence String
-  deriving (Show, Eq)
+newtype ControlSequence = ControlSequence String
+    deriving (Show, Eq)
 
 data ControlSequenceLike
-  = ActiveCharacter CharCode
-  | ControlSequenceProper ControlSequence
-  deriving (Show, Eq)
+    = ActiveCharacter CharCode
+    | ControlSequenceProper ControlSequence
+    deriving (Show, Eq)
 
 instance Hashable ControlSequenceLike where
-  hashWithSalt s (ControlSequenceProper cs) = hashWithSalt s cs
-  hashWithSalt s (ActiveCharacter cc) = hashWithSalt s cc
+    hashWithSalt s (ControlSequenceProper cs) = hashWithSalt s cs
+    hashWithSalt s (ActiveCharacter cc) = hashWithSalt s cc
 
 instance Hashable ControlSequence where
-  hashWithSalt s (ControlSequence w) = hashWithSalt s w
+    hashWithSalt s (ControlSequence w) = hashWithSalt s w
 
 -- Not all Catcodes make it past the lexer, which we can represent in the
 -- type system.
 data LexCatCode
-  = BeginGroup
-  | EndGroup
-  | MathShift
-  | AlignTab
-  | Parameter
-  | Superscript
-  | Subscript
-  | Space
-  | Letter
-  | Other
-  | Active
-  deriving (Show, Eq)
+    = BeginGroup
+    | EndGroup
+    | MathShift
+    | AlignTab
+    | Parameter
+    | Superscript
+    | Subscript
+    | Space
+    | Letter
+    | Other
+    | Active
+    deriving (Show, Eq)
 
 data CharCat = CharCat
-  { char :: CharCode
-  , cat :: LexCatCode
-  } deriving (Show, Eq)
+    { char :: CharCode
+    , cat  :: LexCatCode
+    } deriving (Show, Eq)
 
 data Token
-  = CharCatToken CharCat
-  | ControlSequenceToken ControlSequence
-  deriving (Show, Eq)
+    = CharCatToken CharCat
+    | ControlSequenceToken ControlSequence
+    deriving (Show, Eq)
 
 instance Ord Token where
-  compare _ _ = EQ
+    compare _ _ = EQ
 
 data LexState
-  = SkippingBlanks
-  | LineMiddle
-  | LineBegin
-  deriving (Eq, Show)
+    = SkippingBlanks
+    | LineMiddle
+    | LineBegin
+    deriving (Eq, Show)
 
 spaceTok :: Token
 spaceTok = CharCatToken $ CharCat ' ' Space
@@ -66,9 +65,10 @@ spaceTok = CharCatToken $ CharCat ' ' Space
 chopDropWhile :: ([b] -> Maybe (a, [b])) -> (a -> Bool) -> [b] -> [b]
 chopDropWhile _       _    [] = []
 chopDropWhile getNext test cs = case getNext cs of
-  Nothing -> []
-  Just (next, rest) ->
-    if test next then chopDropWhile getNext test rest else rest
+    Nothing         -> []
+    Just (next, rest)
+        | test next -> chopDropWhile getNext test rest
+        | otherwise -> rest
 
 -- Given a function that chops one 'a' from [b], and a stopping criterion on
 -- 'a', get [a] up to but not including the succeeding item, and the rest of
@@ -78,87 +78,89 @@ chopBreak get test cs = revFirst $ inner [] get cs
   where
     revFirst (a, b) = (reverse a, b)
 
-    inner acc _       [] = (acc, [])
+    inner acc _       []  = (acc, [])
     inner acc getNext cs' = case getNext cs' of
-      Nothing           -> (acc, [])
-      Just (next, rest) -> if test next
-        then (acc, cs')
-        else inner (next : acc) getNext rest
+        Nothing         -> (acc, [])
+        Just (next, rest)
+            | test next -> (acc, cs')
+            | otherwise -> inner (next : acc) getNext rest
 
 extractToken
-  :: ([CharCode] -> Maybe (Cat.CharCat, [CharCode]))
-  -> LexState
-  -> [CharCode]
-  -> Maybe (Token, LexState, [CharCode])
+    :: ([CharCode] -> Maybe (Cat.CharCat, [CharCode]))
+    -> LexState
+    -> [CharCode]
+    -> Maybe (Token, LexState, [CharCode])
 extractToken _     _     [] = Nothing
-extractToken getCC state cs = do
-  (cc1, rest) <- getCC cs
-  extractTokenRest cc1 rest
- where
-  extractTokenRest (Cat.CharCat n cat1) rest
-    -- Control sequence: Grab it.
-    | Cat.Escape <- cat1 = do
-      (cc2@(Cat.CharCat _ cat2), rest') <- getCC rest
-      let (controlCCs, rest2) = if cat2 == Cat.Letter
-            then
-              let (cwNameCCsRest, rest'') =
-                    chopBreak getCC ((/= Cat.Letter) . Cat.cat) rest'
-              in  (cc2 : cwNameCCsRest, rest'')
-            else ([cc2], rest')
-          nextState = case cat2 of
-            Cat.Space  -> SkippingBlanks
-            Cat.Letter -> SkippingBlanks
-            _          -> LineMiddle
-      pure
-        ( ControlSequenceToken $ ControlSequence $ fmap Cat.char controlCCs
-        , nextState
-        , rest2
-        )
-    -- Comment: Ignore rest' of line and switch to line-begin.
-    | Cat.Comment <- cat1
-      = extractToken getCC LineBegin $ chopDropWhile getCC ((/= Cat.EndOfLine) . Cat.cat) rest
-    -- Space at the start of a line: Ignore.
-    | LineBegin <- state, Cat.Space <- cat1
-      = extractToken getCC state rest
-    -- Empty line: Make a paragraph.
-    | LineBegin <- state, Cat.EndOfLine <- cat1
-      = pure (ControlSequenceToken $ ControlSequence "par", LineBegin, rest)
-    -- Simple tokeniser cases.
-    | Cat.BeginGroup <- cat1
-      = pure (CharCatToken $ CharCat n BeginGroup, LineMiddle, rest)
-    | Cat.EndGroup <- cat1
-      = pure (CharCatToken $ CharCat n EndGroup, LineMiddle, rest)
-    | Cat.MathShift <- cat1
-      = pure (CharCatToken $ CharCat n MathShift, LineMiddle, rest)
-    | Cat.AlignTab <- cat1
-      = pure (CharCatToken $ CharCat n AlignTab, LineMiddle, rest)
-    | Cat.Parameter <- cat1
-      = pure (CharCatToken $ CharCat n Parameter, LineMiddle, rest)
-    | Cat.Superscript <- cat1
-      = pure (CharCatToken $ CharCat n Superscript, LineMiddle, rest)
-    | Cat.Subscript <- cat1
-      = pure (CharCatToken $ CharCat n Subscript, LineMiddle, rest)
-    | Cat.Letter <- cat1
-      = pure (CharCatToken $ CharCat n Letter, LineMiddle, rest)
-    | Cat.Other <- cat1
-      = pure (CharCatToken $ CharCat n Other, LineMiddle, rest)
-    | Cat.Active <- cat1
-      = pure (CharCatToken $ CharCat n Active, LineMiddle, rest)
-    -- Space, or end of line, while skipping blanks: Ignore.
-    | SkippingBlanks <- state, Cat.Space <- cat1
-      = extractToken getCC state rest
-    | SkippingBlanks <- state, Cat.EndOfLine <- cat1
-      = extractToken getCC  state rest
-    -- Space in middle of line: Make a space token and start skipping blanks.
-    | LineMiddle <- state, Cat.Space <- cat1
-      = pure (spaceTok, SkippingBlanks, rest)
-    -- End of line in middle of line: Make a space token and go to line begin.
-    | LineMiddle <- state, Cat.EndOfLine <- cat1
-      = pure (spaceTok, LineBegin, rest)
-    -- Ignored: Ignore.
-    | Cat.Ignored <- cat1
-      = extractToken getCC state rest
-    -- Invalid: Print error message and ignore.
-    -- TODO: TeXbook says to print an error message in this case.
-    | Cat.Invalid <- cat1
-      = extractToken getCC state rest
+extractToken getCC state cs =
+    do
+    (cc1, rest) <- getCC cs
+    extractTokenRest cc1 rest
+  where
+    extractTokenRest (Cat.CharCat n cat1) rest = case (cat1, state) of
+        -- Control sequence: Grab it.
+        (Cat.Escape, _) ->
+            do
+            (cc2@(Cat.CharCat _ cat2), rest') <- getCC rest
+            let (controlCCs, rest2) = if cat2 == Cat.Letter
+                    then
+                        let (cwNameCCsRest, rest'') =
+                                chopBreak getCC ((/= Cat.Letter) . Cat.cat) rest'
+                        in  (cc2 : cwNameCCsRest, rest'')
+                    else
+                        ([cc2], rest')
+                nextState = case cat2 of
+                    Cat.Space  -> SkippingBlanks
+                    Cat.Letter -> SkippingBlanks
+                    _          -> LineMiddle
+            pure ( ControlSequenceToken $ ControlSequence $ fmap Cat.char controlCCs
+                 , nextState
+                 , rest2
+                 )
+        -- Comment: Ignore rest' of line and switch to line-begin.
+        (Cat.Comment, _) ->
+            extractToken getCC LineBegin $ chopDropWhile getCC ((/= Cat.EndOfLine) . Cat.cat) rest
+        -- Space at the start of a line: Ignore.
+        (Cat.Space, LineBegin) ->
+            extractToken getCC state rest
+        -- Empty line: Make a paragraph.
+        (Cat.EndOfLine, LineBegin) ->
+            pure (ControlSequenceToken $ ControlSequence "par", LineBegin, rest)
+        -- Simple tokeniser cases.
+        (Cat.BeginGroup, _) ->
+            pure (CharCatToken $ CharCat n BeginGroup, LineMiddle, rest)
+        (Cat.EndGroup, _) ->
+            pure (CharCatToken $ CharCat n EndGroup, LineMiddle, rest)
+        (Cat.MathShift, _) ->
+            pure (CharCatToken $ CharCat n MathShift, LineMiddle, rest)
+        (Cat.AlignTab, _) ->
+            pure (CharCatToken $ CharCat n AlignTab, LineMiddle, rest)
+        (Cat.Parameter, _) ->
+            pure (CharCatToken $ CharCat n Parameter, LineMiddle, rest)
+        (Cat.Superscript, _) ->
+            pure (CharCatToken $ CharCat n Superscript, LineMiddle, rest)
+        (Cat.Subscript, _) ->
+            pure (CharCatToken $ CharCat n Subscript, LineMiddle, rest)
+        (Cat.Letter, _) ->
+            pure (CharCatToken $ CharCat n Letter, LineMiddle, rest)
+        (Cat.Other, _) ->
+            pure (CharCatToken $ CharCat n Other, LineMiddle, rest)
+        (Cat.Active, _) ->
+            pure (CharCatToken $ CharCat n Active, LineMiddle, rest)
+        -- Space, or end of line, while skipping blanks: Ignore.
+        (Cat.Space, SkippingBlanks) ->
+            extractToken getCC state rest
+        (Cat.EndOfLine, SkippingBlanks) ->
+            extractToken getCC  state rest
+        -- Space in middle of line: Make a space token and start skipping blanks.
+        (Cat.Space, LineMiddle) ->
+            pure (spaceTok, SkippingBlanks, rest)
+        -- End of line in middle of line: Make a space token and go to line begin.
+        (Cat.EndOfLine, LineMiddle) ->
+            pure (spaceTok, LineBegin, rest)
+        -- Ignored: Ignore.
+        (Cat.Ignored, _) ->
+            extractToken getCC state rest
+        -- Invalid: Print error message and ignore.
+        -- TODO: TeXbook says to print an error message in this case.
+        (Cat.Invalid, _) ->
+            extractToken getCC state rest
