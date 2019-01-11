d33d4435267490886c1b266dd03b33bea8a35407
Elliot Marsden <elliot.marsden@gmail.com>
2019-01-11 12:44:54 +0100

    Reformat var-assignment module

--- src/HeX/Parse/VarAssignment.hs
+++ src/HeX/Parse/VarAssignment.hs
@@ -15,42 +15,44 @@ import           HeX.Parse.Length
 -- AST.
 
 data VariableAssignment
-  = IntegerVariableAssignment IntegerVariable Number
-  | LengthVariableAssignment LengthVariable Length
-  deriving (Show)
+    = IntegerVariableAssignment IntegerVariable Number
+    | LengthVariableAssignment LengthVariable Length
+    deriving (Show)
 
 data QuantVariable a
-  = ParamVar a
-  | TokenVar String
-  | RegisterVar Number
-  deriving (Show)
+    = ParamVar a
+    | TokenVar String
+    | RegisterVar Number
+    deriving (Show)
 
 data IntegerVariable = IntegerVariable (QuantVariable T.IntegerParameter)
-  deriving (Show)
+    deriving (Show)
 data LengthVariable = LengthVariable (QuantVariable T.LengthParameter)
-  deriving (Show)
+    deriving (Show)
 
 parseVariableAssignment :: SimpExpandParser VariableAssignment
-parseVariableAssignment
-  = P.choice [ parseQuantityAssignment parseIntegerVariable parseNumber IntegerVariableAssignment
-             , parseQuantityAssignment parseLengthVariable parseLength LengthVariableAssignment ]
+parseVariableAssignment =
+    P.choice [ parseQuantityAssignment parseIntegerVariable parseNumber IntegerVariableAssignment
+             , parseQuantityAssignment parseLengthVariable parseLength LengthVariableAssignment
+             ]
   where
     parseQuantityAssignment varParser valParser f =
-      f <$> varParser <* skipOptionalEquals <*> valParser
+        f <$> varParser <* skipOptionalEquals <*> valParser
 
 parseQuantityVariable
-  :: (T.PrimitiveToken -> Maybe p) -- Try to extract a parameter from a token.
-  -> (T.PrimitiveToken -> Maybe String) -- Try to extract a short-def token name from a token.
-  -> T.PrimitiveToken -- A token signifying the start of a relevant register address.
-  -> SimpExpandParser (QuantVariable p)
-parseQuantityVariable getParam getTok regHead
-  = P.choice [ ParamVar <$> satisfyThen getParam
+    :: (T.PrimitiveToken -> Maybe p) -- Try to extract a parameter from a token.
+    -> (T.PrimitiveToken -> Maybe String) -- Try to extract a short-def token name from a token.
+    -> T.PrimitiveToken -- A token signifying the start of a relevant register address.
+    -> SimpExpandParser (QuantVariable p)
+parseQuantityVariable getParam getTok regHead =
+    P.choice [ ParamVar <$> satisfyThen getParam
              , TokenVar <$> satisfyThen getTok
-             , RegisterVar <$> (skipSatisfiedEquals regHead >> parseNumber) ]
+             , RegisterVar <$> (skipSatisfiedEquals regHead >> parseNumber)
+             ]
 
 parseIntegerVariable :: SimpExpandParser IntegerVariable
 parseIntegerVariable =
-  IntegerVariable <$> parseQuantityVariable getParam getTok (T.RegisterVariableTok T.RegInt)
+    IntegerVariable <$> parseQuantityVariable getParam getTok (T.RegisterVariableTok T.RegInt)
   where
     getParam (T.IntParamVarTok p) = Just p
     getParam _ = Nothing
@@ -60,7 +62,7 @@ parseIntegerVariable =
 
 parseLengthVariable :: SimpExpandParser LengthVariable
 parseLengthVariable =
-  LengthVariable <$> parseQuantityVariable getParam getTok (T.RegisterVariableTok T.RegLen)
+    LengthVariable <$> parseQuantityVariable getParam getTok (T.RegisterVariableTok T.RegLen)
   where
     getParam (T.LenParamVarTok p) = Just p
     getParam _ = Nothing
